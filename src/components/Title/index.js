c638865e6dae79d782cd28185f75a9ed2e9533a6
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2022-07-06 16:51:52 -0300

    Update index.js

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ import styles from './style';
 export default function Title() {
     return (
         <View style={styles.boxTitle}>
-            <Text style={styles.textTitle}>CALCULADORA IMC</Text>
+            <Text style={styles.textTitle}>CALCULO IMC</Text>
         </View>
     );
 }
